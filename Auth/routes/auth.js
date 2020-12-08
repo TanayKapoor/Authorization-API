534874b3062d2a543674e765bc0ad94f8673f5f5
tanaykapoor <itstkaay@gmail.com>
2020-12-08 18:35:02 +0530

    Added validation

--- Auth/routes/auth.js
+++ Auth/routes/auth.js
@@ -1,18 +1,32 @@
+const Joi = require('joi');
+
+const schema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().lowercase().required(),
+    password: Joi.string().min(8).required()
+});
+
 const router = require('express').Router();
 const User = require('../model/User');
 
 router.post('/register',async (req, res) => {
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
-    try{
-        const savedUser = await user.save();
-        res.send(savedUser);
-    }catch(err){
-        res.status(400).send(err);
-    }
+
+    // Validate
+    const validation = schema.validate(req.body);
+    res.send(validation);
+
+
+    // const user = new User({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: req.body.password
+    // });
+    // try{
+    //     const savedUser = await user.save();
+    //     res.send(savedUser);
+    // }catch(err){
+    //     res.status(400).send(err);
+    // }
 });
 
 module.exports = router;
