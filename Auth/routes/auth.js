be533f38979175eeb30925eb837e7ab4e239bf12
tanaykapoor <itstkaay@gmail.com>
2020-12-08 18:55:52 +0530

    Added error message to validation

--- Auth/routes/auth.js
+++ Auth/routes/auth.js
@@ -12,21 +12,21 @@ const User = require('../model/User');
 router.post('/register',async (req, res) => {
 
     // Validate
-    const validation = schema.validate(req.body);
-    res.send(validation);
+    const {error} = schema.validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
 
 
-    // const user = new User({
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     password: req.body.password
-    // });
-    // try{
-    //     const savedUser = await user.save();
-    //     res.send(savedUser);
-    // }catch(err){
-    //     res.status(400).send(err);
-    // }
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try{
+        const savedUser = await user.save();
+        res.send(savedUser);
+    }catch(err){
+        res.status(400).send(err);
+    }
 });
 
 module.exports = router;
