071e1dcbfb03cb6367d8a08a0ffc093278602a92
tanaykapoor <itstkaay@gmail.com>
2020-12-09 12:46:00 +0530

    Added duplicate user check

--- Auth/routes/auth.js
+++ Auth/routes/auth.js
@@ -8,6 +8,10 @@ router.post('/register',async (req, res) => {
     const {error} = registerValidation(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
+    // Checking for duplicate user
+    const emailExist = await User.findOne({email: req.body.email});
+    if(emailExist) return res.status(400).send('Email already exists');
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
