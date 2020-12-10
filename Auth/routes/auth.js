dedf400f61a770da7fe402e0a5d3a59296064210
tanaykapoor <itstkaay@gmail.com>
2020-12-10 13:40:18 +0530

    Added Password Hashing

--- Auth/routes/auth.js
+++ Auth/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 const {registerValidation} = require('../validation');
 
 
@@ -12,10 +13,14 @@ router.post('/register',async (req, res) => {
     const emailExist = await User.findOne({email: req.body.email});
     if(emailExist) return res.status(400).send('Email already exists');
 
+    // Hashing Password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashPassword
     });
     try{
         const savedUser = await user.save();
